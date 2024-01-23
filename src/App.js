443eecf9c096b84a0b1560a847a2111660035447
kamindu.g <kamindu.g@informaticsint.com>
2024-01-23 22:53:14 +0530

    feat: screen desc:: user profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// import { ProductList } from "./pages";
 import AddProduct from "./pages/ProductManagement/AddProduct/AddProduct";
 import UpdateProducts from "./pages/ProductManagement/UpdateProduct/UpdateProducts";
 import ProductList from "./pages/ProductManagement/ProductList/ProductList";
 import UserRegistration from "./pages/UserManagement/UserRegistration";
 import UserLogin from "./pages/UserManagement/UserLogin";
+import UserProfile from "./pages/UserManagement/UserProfile";
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div >
@@ -20,6 +22,7 @@ function App() {
 
             <Route path="/user-management/register" element={<UserRegistration />} />
             <Route path="/user-management/login" element={<UserLogin />} />
+            <Route path="/user-management/user-profile" element={<UserProfile />} />
           </Routes>
         </Router>
       </div>
