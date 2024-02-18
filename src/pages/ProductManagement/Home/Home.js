805547f49d2223d47bbcad23459e5a1865022366
CKariyawasam <cgamage2023@gmail.com>
2024-02-19 03:15:53 +0530

    inserted a carousel contaianer for the home and added 2 extra button.ongoing

--- src/pages/ProductManagement/Home/Home.js
+++ src/pages/ProductManagement/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Home.css';
+import Carousel from '../../../components/Carousel/Carousel';
 function Home() {
     return (
         <div className='home'>
 
             <div className="homeimage">
                 <div className=".home .container">
-                    <button class="btn">Add Product</button>
-
+                    <button class="btn">Admin</button>
+                    <button class="btn">User</button>
+                    <Carousel />
                 </div>
             </div>
         </div>
